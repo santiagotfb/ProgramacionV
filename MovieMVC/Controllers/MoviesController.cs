49a985e9efc519bc1705a75233ba77c09968e4c8
santiagotfb <santiagofbecco@gmail.com>
2023-05-10 23:42:41 -0300

    S4

---
+++ MovieMVC/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MovieMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
--- MovieMVC/Program.cs
+++ MovieMVC/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieMVC.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MovieMVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieMVCContext") ?? throw new InvalidOperationException("Connection string 'MovieMVCContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
